7d93bd95a2568fc1a4cf5bd57f0491e4eba1a059
Eduardo Cordovil <86244886+EduardoSilvaCordovil@users.noreply.github.com>
2024-04-08 20:45:11 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dbConnection = require('../database/db');
 const app = express();
-const bodyParser = require('bodyParser');
+//const bodyParser = require('bodyParser');
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 dbConnection;
+
+// ROTA DE TESTE
+app.use("/", (req, res) => {
+    res.send('<h1>COMPACINE</h1>');
+});
 
 app.listen(port, () => {
   console.log(`SERVER IS RUNNING ON http://localhost: ${port}`);
