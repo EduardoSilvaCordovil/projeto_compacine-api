b77f0321a991bb1ec3b07cc6a3697e29420b2cdd
Emmanuel-Moraes <emmanuel.moraes.pb@compasso.com.br>
2024-04-09 16:35:58 -0300

    feat: connection working on app

--- src/app.js
+++ src/app.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const dbConnection = require('../database/db');
 const ticketRoutes = require('./routes/ticket.routes');
-const app = express();
 require('dotenv').config();
 //const bodyParser = require('bodyParser');
+const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
-app.use(express.json());
 dbConnection();
 
-// ROTA DE TESTE
-app.use('/', (req, res) => {
-  res.send('<h1>COMPACINE</h1>');
-});
+app.use(express.json());
 
-app.use('/api/tickets', ticketRoutes);
+app.use('/tickets', ticketRoutes);
 
 app.listen(port, () => {
   console.log(`SERVER IS RUNNING ON http://localhost:${port}`);
