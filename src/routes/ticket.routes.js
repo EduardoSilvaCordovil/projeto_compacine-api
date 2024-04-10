5a79bd0fd282397e4010c3506be1b1c201662a32
Juan Victor <juanvictoficial@gmail.com>
2024-04-10 15:12:45 -0300

    Routes for CRUD operations of tickets

--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -3,7 +3,9 @@ const TicketController = require('../controllers/ticket.controller');
 
 const router = express.Router();
 
-router.post('/', TicketController.createTicket);
-router.get('/', TicketController.getTickets);
-
+router.post('/tickets', TicketController.createTicket);
+router.get('/tickets', TicketController.getTickets);
+router.get('/tickets:id', TicketController.getTicket);
+router.put('/tickets:id', TicketController.uptadeTicket);
+router.delete('/tickets:id', TicketController.deleteTicket);
 module.exports = router;
