e81b6a68597268ef5889dd96268ec4f9fc15d4fb
Juan Victor <juanvictoficial@gmail.com>
2024-04-10 12:33:04 -0300

    asynchronous functions for ticket handling

--- src/services/ticket.service.js
+++ src/services/ticket.service.js
@@ -10,6 +10,32 @@ class TicketService {
   async getTickets() {
     return TicketModel.find();
   }
+
+  async getTicket(id) {
+    const ticket = TicketModel.findById(id);
+    if (!ticket || ticket === null) {
+      throw new Error('Ticket não encontrado');
+    }
+    return ticket;
+  }
+
+  async updateTicket(id, ticket) {
+    const updatedTicket = await TicketModel.findByIdAndpdate(id, ticket, {
+      new: true,
+    });
+    if (!updatedTicket) {
+      throw new Error('Ticket não encontrado');
+    }
+    return updatedTicket;
+  }
+
+  async deleteTicket(id) {
+    const ticket = await TicketModel.findByIdAndDelete(id);
+    if (!ticket) {
+      throw new Error('Ticket não encontrado');
+    }
+    return ticket;
+  }
 }
 
 module.exports = new TicketService();
