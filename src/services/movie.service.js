298ff5a3678a9943a7a8aef8a7eb22eaecc86e7d
Ian Pessoa <ian.pessoaa@gmail.com>
2024-04-10 16:44:56 -0300

    creating movie services

---
+++ src/services/movie.service.js
@@ -0,0 +1,45 @@
+const MovieModel = require('../models/movie');
+
+class MovieService {
+  async createMovie(movie) {
+    const newMovie = new MovieModel(data);
+    await newMovie.save();
+    return newMovie;
+  }
+
+  async getMovies() {
+    return MovieModel.find();
+  }
+
+  async getMovie(id) {
+    const movie = MovieModel.findById(id);
+
+    if(!movie) {
+      throw new Error('Movie not found');
+    }
+
+    return movie;
+  }
+
+  async updateMovie(id, movie) {
+    const updatedMovie = await MovieModel.findByIdAndUpdate(id, movie, { new:true });
+
+    if(!updatedMovie) {
+      throw new Error('Movie not found');
+    }
+
+    return updatedMovie;
+  }
+
+  async deleteMovie(id) {
+    const movie = MovieModel.findByIdAndDelete(id);
+
+    if(!car) {
+      throw new Error('Movie not found');
+    }
+
+    return movie;
+  }
+}
+
+module.exports = new MovieService();
