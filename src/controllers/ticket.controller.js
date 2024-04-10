db78fe6962c63a9b2ca4dc2217ac998f867306c8
Juan Victor <juanvictoficial@gmail.com>
2024-04-10 14:32:33 -0300

    Ticket route handlers update

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -18,6 +18,33 @@ class TicketController {
       res.status(500).json({ error: err.message });
     }
   }
+
+  async getTicket(req, res) {
+    try {
+      const ticket = await TicketService.getTicket(req.params.id);
+      res.status(200).json(ticket);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async uptadeTicket(req, res) {
+    try {
+      const ticket = await TicketService.updateTicket(req.params.id, req.body);
+      res.status(200).json(car);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async deleteTicket(req, res) {
+    try {
+      const ticket = await TicketService.deleteTicket(req.params.id);
+      res.status(200).json(ticket);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
 }
 
 module.exports = new TicketController();
